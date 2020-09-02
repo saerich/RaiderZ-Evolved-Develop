6ffa446fdd4c8b23e9e1e261929a97a50340651e
MrFarbodD <fabs1996@live.co.uk>
2020-09-02 23:59:55 +0200

    Updated Mcv Project Settings - Updated Composite project intermediate output directory. - Removed Composite project from the Mcv solution as it was not required. - Added an output directory for obj and idb files. This resolves the issue with error code C2859 - Added a default output directory for PDB file. - Enabled multi-processor compilation. - Removed unnecessary defines in the StdAfx.h file.

--- mdk/Tools/McvRS3/StdAfx.h
+++ mdk/Tools/McvRS3/StdAfx.h
@@ -3,12 +3,7 @@
 //      are changed infrequently
 //
 
-#if !defined(AFX_STDAFX_H__509527B0_3A96_4ED7_9757_034DBBB92111__INCLUDED_)
-#define AFX_STDAFX_H__509527B0_3A96_4ED7_9757_034DBBB92111__INCLUDED_
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
 
 #define _CRT_SECURE_NO_DEPRECATE
 
@@ -72,5 +67,3 @@
 
 //{{AFX_INSERT_LOCATION}}
 // Microsoft Visual C++ will insert additional declarations immediately before the previous line.
-
-#endif // !defined(AFX_STDAFX_H__509527B0_3A96_4ED7_9757_034DBBB92111__INCLUDED_)
